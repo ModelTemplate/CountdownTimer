b17e1ad3712831b9af061997ab93d88198ff5f7b
Thomas <thmsphung2000@gmail.com>
2021-02-04 13:46:20 -0800

    Minor

--- CountdownTest.js
+++ CountdownTest.js
@@ -3,23 +3,23 @@
 // https://www.markhansen.co.nz/javascript-optional-parameters/
 // https://stackoverflow.com/questions/50715033/javascript-constructor-with-optional-parameters
 
-// generateCountdownHTML(new Countdown());
-// generateCountdownHTML(new Countdown({ loopTime: "10s" }), "Testing 10 second loop for ∞ times");
-// generateCountdownHTML(new Countdown({ loopTime: "1m" }), "Testing one minute loop for ∞ times");
-// generateCountdownHTML(new Countdown({ loopTime: "1h" }), "Testing one hour loop for ∞ times");
-// generateCountdownHTML(new Countdown({ loopTime: "1D", loopLimit: 1000 }), "Testing one day loop for 1000 times");
-// generateCountdownHTML(new Countdown({ loopTime: "1M", loopLimit: 1000 }), "Testing one month loop for 1000 times");
-// generateCountdownHTML(new Countdown({ loopTime: "1Y", loopLimit: 1000 }), "Testing one year loop for 1000 times");
-// generateCountdownHTML(new Countdown({ loopTime: "10s", loopLimit: 0 }), "Testing 10 second loop for 0 times");
-// generateCountdownHTML(new Countdown({ 
-//     seedDate: "January 1, 2021 00:00:00 UTC", loopTime: "2Y", loopLimit: 1000, bText: "Countdown (", aText: ")", 
-//     dateFormat: "YY", dateLabels: "full"
-// }), "Testing formatting");
-// let start = new Date("January 1, 2021 00:00:00 UTC");
-// let end = new Date();
-// let diff = end.getTime() - start.getTime();
-// generateCountdownHTML(new Countdown({ seedDate: start.toISOString(), loopTime: "1D", loopLimit: (diff / 86400000) + 1 }), 
-//         "Testing one day loop starting at 1/1/2021 and ending one day from today (UTC)");
+generateCountdownHTML(new Countdown());
+generateCountdownHTML(new Countdown({ loopTime: "10s" }), "Testing 10 second loop for ∞ times");
+generateCountdownHTML(new Countdown({ loopTime: "1m" }), "Testing one minute loop for ∞ times");
+generateCountdownHTML(new Countdown({ loopTime: "1h" }), "Testing one hour loop for ∞ times");
+generateCountdownHTML(new Countdown({ loopTime: "1D", loopLimit: 1000 }), "Testing one day loop for 1000 times");
+generateCountdownHTML(new Countdown({ loopTime: "1M", loopLimit: 1000 }), "Testing one month loop for 1000 times");
+generateCountdownHTML(new Countdown({ loopTime: "1Y", loopLimit: 1000 }), "Testing one year loop for 1000 times");
+generateCountdownHTML(new Countdown({ loopTime: "10s", loopLimit: 0 }), "Testing 10 second loop for 0 times");
+generateCountdownHTML(new Countdown({ 
+    seedDate: "January 1, 2021 00:00:00 UTC", loopTime: "2Y", loopLimit: 1000, bText: "Countdown (", aText: ")", 
+    dateFormat: "YY", dateLabels: "full"
+}), "Testing formatting");
+let start = new Date("January 1, 2021 00:00:00 UTC");
+let end = new Date();
+let diff = end.getTime() - start.getTime();
+generateCountdownHTML(new Countdown({ seedDate: start.toISOString(), loopTime: "1D", loopLimit: (diff / 86400000) + 1 }), 
+        "Testing one day loop starting at 1/1/2021 and ending one day from today (UTC)");
 generateCountdownHTML(new Countdown({ loopTime: "30s", delayTime: "10s" }), "Testing 10 second delay timer");
 generateCountdownHTML(new Countdown({ loopTime: "30s", delayTime: "10s", delayDisplay: false }), "Testing hiding the delay timer");
 
